7f0ce2cf89279e5328a01ae533dfbab7c8ee9746
Cora-Hansen <znk562@vols.utk.edu>
2022-05-02 16:22:14 -0400

    Cleaning & Creating Train and Test
    I created the tsibble, differenced the data, and created the train and test data

--- Models.R
+++ Models.R
@@ -2,3 +2,38 @@ library(fpp3)
 
 credit <- read.csv("credit.csv")
 
+#Making data increasing
+creditIncr <- transform(credit, credit_in_millions = rev(credit_in_millions))
+
+#Creating tsibble
+creditIncr %>% 
+  mutate(Month = seq(as.Date("1981/1/1"), by = "month", length.out = nrow(creditIncr))) %>%
+  mutate(Month = yearmonth(Month)) %>% 
+  as_tsibble(index = Month) -> TSCredit
+
+TSCredit %>% 
+  autoplot()
+
+#Differencing
+TSCredit %>%
+  features(credit_in_millions, unitroot_kpss)
+
+TSCredit %>%
+  features(credit_in_millions, unitroot_ndiffs)
+
+TSCredit_Diff <- TSCredit %>%
+  mutate(y = box_cox(credit_in_millions, lambda)) %>% 
+  mutate(y = difference(y, 12)) %>% 
+  mutate(y = difference(y))
+
+TSCredit_Diff %>% 
+  autoplot(y)
+
+#Creating Train and Test Data
+train <- TSCredit_Diff %>% 
+  filter(Month < yearmonth("2021 Jan"))
+
+test <- TSCredit_Diff %>% 
+  filter(Month >= yearmonth("2021 Jan"))
+
+#Models
